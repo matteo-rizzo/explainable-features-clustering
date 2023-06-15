2b02dc63d5c8ff7a4582024e5bcf992799b9074e
Matteo Marcuzzo <matteo.marcuzzo.it@gmail.com>
2023-06-15 10:10:16 +0200

    Testing on whole dataset for mercury

--- wip/food/classification_playground.py
+++ wip/food/classification_playground.py
@@ -95,18 +95,28 @@ def simple_for():
         train_config = yaml.safe_load(f)
     with open('config/training/hypeparameter_configuration.yaml', 'r') as f:
         hyp = yaml.safe_load(f)
-    train_subset, test_subset = create_stratified_splits(Food101Dataset(train=True, augment=False),
-                                                         n_splits=1,
-                                                         train_size=10100,
-                                                         test_size=1010,)
-    train = torch.utils.data.DataLoader(train_subset,
+    # train_subset, test_subset = create_stratified_splits(Food101Dataset(train=True, augment=False),
+    #                                                      n_splits=1,
+    #                                                      train_size=10100,
+    #                                                      test_size=1010,)
+    # train = torch.utils.data.DataLoader(train_subset,
+    #                                     batch_size=train_config["batch_size"],
+    #                                     shuffle=True,
+    #                                     num_workers=train_config["workers"],
+    #                                     drop_last=True)
+    # test = torch.utils.data.DataLoader(test_subset,
+    #                                    batch_size=train_config["batch_size"],
+    #                                    shuffle=False,
+    #                                    num_workers=train_config["workers"],
+    #                                    drop_last=True)
+    train = torch.utils.data.DataLoader(Food101Dataset(train=True, augment=True),
                                         batch_size=train_config["batch_size"],
                                         shuffle=True,
                                         num_workers=train_config["workers"],
                                         drop_last=True)
-    test = torch.utils.data.DataLoader(test_subset,
+    test = torch.utils.data.DataLoader(Food101Dataset(train=False),
                                        batch_size=train_config["batch_size"],
-                                       shuffle=False,
+                                       shuffle=True,
                                        num_workers=train_config["workers"],
                                        drop_last=True)
 
